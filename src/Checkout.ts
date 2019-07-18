f0319975c63619480b3d267c8846326191186219
Robert Stone <robert-007@hotmail.co.uk>
2019-07-18 10:41:21 +0100

    Added a way to change price with test, had to add tsconfig to get typescript to compile to es6 to use the find function,  Did some refactoring after test extracting a function that lookup an Items price.

--- src/Checkout.ts
+++ src/Checkout.ts
@@ -1,12 +1,22 @@
+import { Item } from "./Item";
+
 export class Checkout implements ICheckout{
     
     private subTotal=0;
-    
+    private readonly items:Item[]
+    constructor(items:Item[]){
+        this.items=items
+    }
     scan(sku: string): void {
-        this.subTotal=50;
+        const price = this.lookupItemPrice(sku);
+        this.subTotal=price;
     }
     getTotalPrice(): number {
         return this.subTotal;
+    }
+
+    private lookupItemPrice(sku:string):number{
+        return this.items.find((item)=>item.sku===sku).price;
     }
 
 }
--- src/checkout.spec.ts
+++ src/checkout.spec.ts
@@ -4,15 +4,24 @@ describe('Checkout',()=>{
 
     describe('getTotalPrice',()=>{
         it('should return zero when no items have been scanned',()=>{
-            const checkout = new Checkout();
+           
+            const checkout = new Checkout([]);
             const result = checkout.getTotalPrice();
             expect(result).toBe(0);
         });
         it('should return 50 when  item A have been scanned',()=>{
-            const checkout = new Checkout();
+            const prices = [{sku:'A',price:50}];
+            const checkout = new Checkout(prices);
             checkout.scan('A');
             const result = checkout.getTotalPrice();
             expect(result).toBe(50);
+        });
+        it('should return 30 when with item A that has a different price',()=>{
+            const prices = [{sku:'A',price:30}];
+            const checkout = new Checkout(prices);
+            checkout.scan('A');
+            const result = checkout.getTotalPrice();
+            expect(result).toBe(30);
         });
     })
 })
